1bc19d88ac05d82649f56f89a789f64713d7a794
Antonio Júnior <juniorssz@gmail.com>
2022-09-16 07:07:51 -0300

    feat(config/paths): Inclusão do path do IBGE

--- ppgmne_prj_prf/config/paths.py
+++ ppgmne_prj_prf/config/paths.py
@@ -7,5 +7,8 @@ PATH_PPGMNE_PRF = PATH_ROOT / "ppgmne_prj_prf"
 
 # Caminhos em data/
 PATH_DATA = PATH_ROOT / "data"
+
 PATH_DATA_PRF = PATH_DATA / "prf"
 PATH_DATA_PRF_CACHE = PATH_DATA_PRF / "cache"
+
+PATH_DATA_IBGE = PATH_DATA / "ibge"
