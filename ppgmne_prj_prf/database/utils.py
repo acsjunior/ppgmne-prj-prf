f51135f5460c0464a0ad38b6c54914f863ebbd01
Antonio Júnior <juniorssz@gmail.com>
2022-09-16 21:21:28 -0300

    feat(database/utils): Inclusão de funções de pré-processamento

--- ppgmne_prj_prf/database/utils.py
+++ ppgmne_prj_prf/database/utils.py
@@ -1,9 +1,11 @@
 import ssl
 import urllib.request as request
 from io import BytesIO
+from typing import Literal, Union
 from zipfile import ZipFile
 
 import pandas as pd
+from unidecode import unidecode
 
 
 def csv_zip_to_df(
@@ -41,3 +43,50 @@ def csv_zip_to_df(
     df = pd.read_csv(csv_file, delimiter=sep, decimal=dec, encoding=encoding)
 
     return df
+
+
+def clean_string(
+    df: pd.DataFrame,
+    target: Union[list, str],
+    mode: Literal["lower", "upper"] = "lower",
+) -> pd.DataFrame:
+    """_summary_
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data frame completo
+    target : Union[list, str]
+        Campo ou lista de campos para limpeza
+    mode : Literal["lower", "upper"], optional
+        Modo de conversão dos caracteres, by default "lower"
+
+    Returns
+    -------
+    pd.DataFrame
+        Data frame tratado
+    """
+    if isinstance(target, str):
+        target = [target]
+    for col in target:
+        df[col] = df[col].apply(unidecode).str.strip().str.lower()
+        if mode == "upper":
+            df[col] = df[col].str.upper()
+    return df
+
+
+def get_decimal_places(col: pd.Series) -> pd.Series:
+    """_summary_
+
+    Parameters
+    ----------
+    col : pd.Series
+        Coluna numérica para contagem de casas decimais
+
+    Returns
+    -------
+    pd.Series
+        Vetor com a contagem de casas decimais de cada elemento
+    """
+    out = col.astype(float).astype(str).str.split(".").str[1].str.len().astype(int)
+    return out
