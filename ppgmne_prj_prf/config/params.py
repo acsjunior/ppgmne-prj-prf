59d100e9b0a4a774974bb29be2f990e922e69063
Antonio Júnior <juniorssz@gmail.com>
2022-09-19 15:06:59 -0300

    feat: Criação de classe para os pontos agregados

--- ppgmne_prj_prf/config/params.py
+++ ppgmne_prj_prf/config/params.py
@@ -15,11 +15,11 @@ STR_COLS_TO_LOWER = [
 STR_COLS_TO_UPPER = ["municipio", "regional", "delegacia", "uop"]
 
 COORDS_MIN_DECIMAL_PLACES = 3
-
 COORDS_PRECISION = 2
 
+CLUSTERING_FEATS = ["point_n_accidents"]
 N_CLUSTERS = 8
-CLUSTERING_FEATS = ["point_acc"]
+CLUSTER_DMAX = {1: 180, 2: 180, 3: 120, 4: 120, 5: 120, 6: 60, 7: 60, 8: 60}
 
 # Parâmetros do IBGE
 IBGE_YEAR = 2019
