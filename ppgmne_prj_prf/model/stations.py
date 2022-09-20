d3302003d75c7cfdc4c33cb3a6b68805c37acd83
Antonio Júnior <juniorssz@gmail.com>
2022-09-20 15:34:16 -0300

    refactor(model/stations) Reorganização do código

--- ppgmne_prj_prf/model/stations.py
+++ ppgmne_prj_prf/model/stations.py
@@ -13,11 +13,34 @@ from ppgmne_prj_prf.utils import concatenate_dict_of_dicts
 
 class Stations:
     def __init__(self, verbose: bool = True, read_cache: bool = False):
+        self.name_raw = "stations_raw"
         self.name = "stations"
         self.uf = UF
         self.df_stations = pd.DataFrame()
         self.verbose = verbose
         self.read_cache = read_cache
+
+    def extract(self):
+        """Método para extração dos dados das estações policiais"""
+
+        logger.info(
+            "Início da leitura dos dados das estações policiais."
+        ) if self.verbose else None
+
+        cache_path = PATH_DATA_PRF_CACHE_DATABASE / f"{self.name_raw}.pkl"
+
+        if self.read_cache:
+            logger.info("Modo de leitura da cache ativo.")
+            self.__geo_stations = pd.read_pickle(cache_path)
+            logger.info("Fim da extração dos dados.")
+            return
+
+        logger.info(
+            "Convertendo os dados das estações policiais para GeoJson."
+        ) if self.verbose else None
+        self.__geo_stations = kml2geojson.main.convert(PATH_DATA_PRF / f"{self.uf}.kml")
+
+        logger.info(f"Fim da extração dos dados.")
 
     def transform(self):
         """Método para o pré-processamento dos dados das estações policiais"""
@@ -32,15 +55,48 @@ class Stations:
             logger.info("Fim do pré-processamento.")
             return
 
-        logger.info(
-            "Convertendo os dados das estações policiais para GeoJson."
-        ) if self.verbose else None
-        self.__stations = kml2geojson.main.convert(PATH_DATA_PRF / f"{self.uf}.kml")
+        df_out = self.__structure_stations().pipe(self.__trace_df)
+
+        logger.info("Incluindo os códigos das UOPs.") if self.verbose else None
+        with open(PATH_DATA_PRF / "transformations.json") as file:
+            stations_to_replace = concatenate_dict_of_dicts(
+                json.load(file)["stations_replace"]
+            )
+        df_out["uop"] = df_out["name"].map(stations_to_replace)
+
+        # Armazena a cache caso o modo de leitura da cache não esteja ativo:
+        if not self.read_cache:
+            logger.info(f"Armazenado {cache_path}.")
+            df_out.to_pickle(cache_path)
+
+        self.df_stations = df_out.pipe(self.__trace_df)
+        logger.info("Fim do pré-processamento.") if self.verbose else None
+
+    ############################################################################################################
+
+    def __trace_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Método para impressão das dimensões do data frame.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Base de dados.
+
+        Returns
+        -------
+        pd.DataFrame
+            Base de dados.
+        """
+        logger.info(f"shape: {df.shape}") if self.verbose else None
+        return df
+
+    def __structure_stations(self) -> pd.DataFrame:
 
         logger.info(
             "Estruturando os dados das estações policiais."
         ) if self.verbose else None
-        dict_out = {
+
+        dict_chars = {
             "type": [],
             "name": [],
             "station_father": [],
@@ -55,9 +111,9 @@ class Stations:
             "longitude": [],
         }
 
-        logger.info("Extraindo as características.") if self.verbose else None
         lst_full_description = []
-        for d in self.__stations[0]["features"]:
+
+        for d in self.__geo_stations[0]["features"]:
             if d["type"] == "Feature":
 
                 # Extrai as informações parcialmente tratadas:
@@ -68,8 +124,8 @@ class Stations:
                 latitude = float(str(d["geometry"]["coordinates"][1]).replace(",", "."))
 
                 # Insere as informações iniciais no dicionário:
-                dict_out["longitude"].append(longitude)
-                dict_out["latitude"].append(latitude)
+                dict_chars["longitude"].append(longitude)
+                dict_chars["latitude"].append(latitude)
 
                 # Insere a descrição completa em uma lista temporária:
                 lst_full_description.append(full_description)
@@ -114,30 +170,17 @@ class Stations:
                     station_code = email_uop.upper().split(".")[0]
 
                 # Insere as informações finais no dicionário:
-                dict_out["type"].append(type)
-                dict_out["name"].append(name)
-                dict_out["station_father"].append(station_father)
-                dict_out["station_code"].append(station_code)
-                dict_out["address"].append(address)
-                dict_out["municipality"].append(municipality)
-                dict_out["state"].append(state)
-                dict_out["phone"].append(phone)
-                dict_out["email_del"].append(email_del)
-                dict_out["email_uop"].append(email_uop)
+                dict_chars["type"].append(type)
+                dict_chars["name"].append(name)
+                dict_chars["station_father"].append(station_father)
+                dict_chars["station_code"].append(station_code)
+                dict_chars["address"].append(address)
+                dict_chars["municipality"].append(municipality)
+                dict_chars["state"].append(state)
+                dict_chars["phone"].append(phone)
+                dict_chars["email_del"].append(email_del)
+                dict_chars["email_uop"].append(email_uop)
 
-        df_out = pd.DataFrame(dict_out)
+        df_out = pd.DataFrame(dict_chars)
 
-        logger.info("Incluindo os códigos das UOPs.") if self.verbose else None
-        with open(PATH_DATA_PRF / "transformations.json") as file:
-            stations_to_replace = concatenate_dict_of_dicts(
-                json.load(file)["stations_replace"]
-            )
-        df_out["uop"] = df_out["name"].map(stations_to_replace)
-
-        # Armazena a cache caso o modo de leitura da cache não esteja ativo:
-        if not self.read_cache:
-            logger.info(f"Armazenado {cache_path}.")
-            df_out.to_pickle(cache_path)
-
-        self.df_stations = df_out
-        logger.info("Fim do pré-processamento.") if self.verbose else None
+        return df_out
