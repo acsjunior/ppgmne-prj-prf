bc06d9914d973c2c0ed07131e5644da1b618009f
Antonio Júnior <juniorssz@gmail.com>
2022-09-19 21:10:00 -0300

    refactor: Reorganização da arquitetura

--- ppgmne_prj_prf/model/stations.py
+++ ppgmne_prj_prf/model/stations.py
@@ -8,7 +8,7 @@ from unidecode import unidecode
 
 from ppgmne_prj_prf.config.params import UF
 from ppgmne_prj_prf.config.paths import PATH_DATA_PRF, PATH_DATA_PRF_CACHE_DATABASE
-from ppgmne_prj_prf.database.utils import concatenate_dict_of_dicts
+from ppgmne_prj_prf.utils import concatenate_dict_of_dicts
 
 
 class Stations:
